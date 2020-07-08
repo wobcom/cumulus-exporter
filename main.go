9a87299ebec89183b4f023606831435629b82d83
fluepke <fabian@luepke.email>
2020-07-08 18:41:00 +0200

    Release version 1.0

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-const version string = "0.1"
+const version string = "1.0"
 
 var (
 	showVersion              = flag.Bool("version", false, "Print version and exit")
